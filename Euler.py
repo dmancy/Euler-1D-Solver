6f47fed398d1025ba8df07b303d483cbc726a9c9
Damien Mancy <damien.mancy@mail.mcgill.ca>
2020-03-01 17:07:37 -0500

    Add Steger_Warming flux

--- Euler.py
+++ Euler.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from General_Riemann import General_Riemann_Problem
 from Flux import Flux
+from Flux_Splitting import Flux_Steger_Warming
 
 class Euler:
 
@@ -14,7 +15,7 @@ class Euler:
 
         self.U_final = None
 
-        self.Solver_Godunov()
+        self.Solver_Steger_Warming()
 
 
     def Solver_Godunov(self):
@@ -68,3 +69,49 @@ class Euler:
             self.U_final = U
 
 
+    def Solver_Steger_Warming(self):
+
+        U = self.U_init.copy()
+        U_new = U.copy()
+
+        t = self.t0
+
+        delta_x = min(self.grid.cell_length)
+
+
+        while t <= self.t_final:
+            #Find max(|U| + c)
+            max_eigen = 0
+            for i_cell in range(len(self.grid.cell_position)):
+                if max_eigen < abs(U[i_cell].velocity) + U[i_cell].c:
+                    umax = abs(U[i_cell].velocity) + U[i_cell].c
+
+
+
+            delta_t = self.Courant_Number * delta_x / umax
+
+            for i_cell in range(len(self.grid.cell_position)):
+
+                if (0 < i_cell < len(self.grid.cell_position)-1):
+                    Flux1 = Flux_Steger_Warming(U[i_cell-1],U[i_cell]) 
+                    Flux2 = Flux_Steger_Warming(U[i_cell],U[i_cell+1]) 
+                elif (i_cell == 0):
+                    Flux1 = Flux_Steger_Warming(U[i_cell],U[i_cell]) 
+                    Flux2 = Flux_Steger_Warming(U[i_cell],U[i_cell+1]) 
+                else:
+                    Flux1 = Flux_Steger_Warming(U[i_cell-1],U[i_cell]) 
+                    Flux2 = Flux_Steger_Warming(U[i_cell],U[i_cell]) 
+
+                U_new[i_cell].solver_step(delta_t, delta_x, Flux1, Flux2)
+
+            U = U_new.copy()
+
+            if (t == self.t_final):
+                break
+
+            t += delta_t
+
+            if ( t > self.t_final):
+                delta_t -= t + delta_t - self.t_final
+
+            self.U_final = U
---
+++ Flux_Splitting.py
@@ -0,0 +1,47 @@
+
+
+def Flux_Splitting_Computation(W, lambda1, lambda2, lambda3):
+
+        f1 = (W.gamma - 1)/ W.gamma * W.rho * lambda1 + W.rho/(2*W.gamma) * lambda2 + W.rho/(2*W.gamma) * lambda3
+        f2 = (W.gamma - 1)/ W.gamma * W.rho * lambda1 * W.velocity + W.rho/(2*W.gamma) * lambda2 * (W.velocity + W.c) + W.rho/(2*W.gamma) * lambda3 * (W.velocity - W.c)
+        f3 = (W.gamma - 1)/ W.gamma * W.rho * lambda1 * W.velocity**2/2 + W.rho/(2*W.gamma) * lambda2 * (W.velocity**2/2 + W.gamma/(W.gamma-1) * W.pressure/W.rho + W.velocity * W.c) + W.rho/(2*W.gamma) * lambda3 * (W.velocity**2/2 + W.gamma/(W.gamma-1) * W.pressure/W.rho + W.velocity * W.c)
+
+        return f1, f2, f3
+
+
+
+
+class Flux_Steger_Warming:
+
+    def __init__(self, W_1, W_2):
+        """Flux Constructor"""
+
+        #F_plus
+        lambda_1 = W_1.velocity
+        lambda_1_plus = max(lambda_1, 0)
+
+        lambda_2 = W_1.velocity + W_1.c
+        lambda_2_plus = max(lambda_2, 0)
+
+        lambda_3 = W_1.velocity - W_1.c
+        lambda_3_plus = max(lambda_3, 0)
+
+        F_plus = Flux_Splitting_Computation(W_1, lambda_1_plus, lambda_2_plus, lambda_3_plus)
+
+
+        #F_minus
+        lambda_1 = W_2.velocity
+        lambda_1_minus = min(lambda_1, 0)
+
+        lambda_2 = W_2.velocity + W_2.c
+        lambda_2_minus = min(lambda_2, 0)
+
+        lambda_3 = W_2.velocity - W_2.c
+        lambda_3_minus = min(lambda_3, 0)
+
+        F_minus = Flux_Splitting_Computation(W_2, lambda_1_minus, lambda_2_minus, lambda_3_minus)
+
+        self.f1 = F_plus[0] + F_minus[0]
+        self.f2 = F_plus[1] + F_minus[1]
+        self.f3 = F_plus[2] + F_minus[2]
+
