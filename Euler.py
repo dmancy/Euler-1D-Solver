8e3cc6341c0f67ccb42eb1eac925e3965579a9c7
Damien Mancy <mancydamien@gmail.com>
2020-03-01 20:29:14 -0500

    Add Van-Leer Flux

--- Euler.py
+++ Euler.py
@@ -3,6 +3,7 @@ import numpy as np
 from General_Riemann import General_Riemann_Problem
 from Flux import Flux
 from Flux_Splitting import Flux_Steger_Warming
+from Flux_Van_Leer import Flux_Van_Leer
 
 class Euler:
 
@@ -15,7 +16,7 @@ class Euler:
 
         self.U_final = None
 
-        self.Solver_Steger_Warming()
+        self.Solver_Van_Leer()
 
 
     def Solver_Godunov(self):
@@ -101,6 +102,55 @@ class Euler:
                 else:
                     Flux1 = Flux_Steger_Warming(U[i_cell-1],U[i_cell]) 
                     Flux2 = Flux_Steger_Warming(U[i_cell],U[i_cell]) 
+
+                U_new[i_cell].solver_step(delta_t, delta_x, Flux1, Flux2)
+
+            U = U_new.copy()
+
+            if (t == self.t_final):
+                break
+
+            t += delta_t
+
+            if ( t > self.t_final):
+                delta_t -= t + delta_t - self.t_final
+
+            self.U_final = U
+
+
+
+    def Solver_Van_Leer(self):
+
+        U = self.U_init.copy()
+        U_new = U.copy()
+
+        t = self.t0
+
+        delta_x = min(self.grid.cell_length)
+
+
+        while t <= self.t_final:
+            #Find max(|U| + c)
+            max_eigen = 0
+            for i_cell in range(len(self.grid.cell_position)):
+                if max_eigen < abs(U[i_cell].velocity) + U[i_cell].c:
+                    umax = abs(U[i_cell].velocity) + U[i_cell].c
+
+
+
+            delta_t = self.Courant_Number * delta_x / umax
+
+            for i_cell in range(len(self.grid.cell_position)):
+
+                if (0 < i_cell < len(self.grid.cell_position)-1):
+                    Flux1 = Flux_Van_Leer(U[i_cell-1],U[i_cell]) 
+                    Flux2 = Flux_Van_Leer(U[i_cell],U[i_cell+1]) 
+                elif (i_cell == 0):
+                    Flux1 = Flux_Van_Leer(U[i_cell],U[i_cell]) 
+                    Flux2 = Flux_Van_Leer(U[i_cell],U[i_cell+1]) 
+                else:
+                    Flux1 = Flux_Van_Leer(U[i_cell-1],U[i_cell]) 
+                    Flux2 = Flux_Van_Leer(U[i_cell],U[i_cell]) 
 
                 U_new[i_cell].solver_step(delta_t, delta_x, Flux1, Flux2)
 
---
+++ Flux_Van_Leer.py
@@ -0,0 +1,52 @@
+class Flux_Van_Leer:
+
+    def __init__(self, W1, W2):
+
+        #Flux +
+
+        Mach = W1.velocity/W1.c
+
+        if (Mach <= -1):
+            Mach_flux_1 = 0
+            Mach_flux_2 = 0
+            Mach_flux_3 = 0
+        elif ( -1 < Mach < 1):
+            Mach_flux_1 = (Mach+1)**2/2
+            Mach_flux_2 = ((Mach+1)/2)**2 * ((W1.gamma-1)*Mach+2)
+            Mach_flux_3 = ((W1.gamma-1)*W1.velocity + 2 * W1.c)**2 * W1.rho * W1.c * Mach_flux_1 / (2 * (W1.gamma+1) * (W1.gamma-1))
+        else:
+            Mach_flux_1 = Mach
+            Mach_flux_2 = W1.gamma * Mach**2 +1
+            Mach_flux_3 = W1.rho * W1.c**3 * Mach * (Mach**2/2 + 1/ (W1.gamma -1))
+
+        f1_plus = W1.rho * W1.c * Mach_flux_1
+        f2_plus = W1.rho * W1.c**2/W1.gamma * Mach_flux_2
+        f3_plus = Mach_flux_3 
+
+
+        #Flux -
+        
+        Mach = W2.velocity/W2.c
+
+        if (Mach <= -1):
+            Mach_flux_1 = Mach 
+            Mach_flux_2 = W2.gamma * Mach**2 + 1
+            Mach_flux_3 = W2.rho * W2.c**3 * Mach * (Mach**2/2 + 1/ (W2.gamma -1))
+
+        elif ( -1 < Mach < 1):
+            Mach_flux_1 = -(Mach-1)**2/2
+            Mach_flux_2 = -((Mach-1)/2)**2 * ((W2.gamma-1)*Mach-2)
+            Mach_flux_3 = ((W2.gamma-1)*W2.velocity - 2 * W2.c)**2 * W2.rho * W2.c * Mach_flux_1 / (2 * (W2.gamma+1) * (W2.gamma-1))
+        else:
+            Mach_flux_1 = 0
+            Mach_flux_2 = 0
+            Mach_flux_3 = 0
+
+        f1_minus = W2.rho * W2.c * Mach_flux_1
+        f2_minus = W2.rho * W2.c**2/W2.gamma * Mach_flux_2
+        f3_minus = Mach_flux_3 
+
+        self.f1 = f1_plus + f1_minus
+        self.f2 = f2_plus + f2_minus
+        self.f3 = f3_plus + f3_minus
+
